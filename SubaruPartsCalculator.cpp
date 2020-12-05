867506b12e96f15e3bcf5d2677f3cfe480f3aa49
Mark F <54504479+MarkFeigleson@users.noreply.github.com>
2020-12-04 20:58:19 -0500

    Add files via upload
    I looked at my old code for this calculator and thought of what I had learned and applied a cleaner way of expressing the method for calculating the numbers via function with args.

--- SubaruPartsCalculator.cpp
+++ SubaruPartsCalculator.cpp
@@ -11,12 +11,24 @@ struct SubaruPart
     int TubsPerSkid;
 };
 
+void SkidMath(int TotalAmount, int Skid, int PiecesPerTub, int TubsPerSkid) 
+{
+    int SkidsOrder = TotalAmount / Skid;
+    int PiecesLooseTubs = (TotalAmount % Skid) / PiecesPerTub;
+    int TotalTubs = TotalAmount / PiecesPerTub;
+    std::cout << "The order has " << SkidsOrder << " Skids plus one skid of " << PiecesLooseTubs << " Tubs. The order will need a total of " << TotalTubs << " Tubs." << std::endl;
+
+}
 
 void SubaruCalc()
 {
     int SubaruProductNo{ 0 };
     std::cout << "Enter the Subaru Product Number ONLY - 21982, 21983, 22175/OCWN, 22176/OGTO, 22177/OVNM, 22178/OQTD,  22182/05W8, or 22183/OSAQ" << std::endl;
-    std::cin >> SubaruProductNo;
+    while (!(std::cin >> SubaruProductNo)) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Invalid input.  Try again: ";
+    }
 
 
     //switch to choose the parts we want and get output to match the amount per tub and skid.
@@ -29,11 +41,7 @@ void SubaruCalc()
         std::cout << "Enter the amount of pieces on the order for 21982" << std::endl;
         std::cin >> Part21982.TotalAmount;
 
-        SkidsOrder = Part21982.TotalAmount / Part21982.Skid;
-        PiecesLooseTubs = (Part21982.TotalAmount % Part21982.Skid) / Part21982.PiecesPerTub;
-        TotalTubs = Part21982.TotalAmount / Part21982.PiecesPerTub;
-
-        std::cout << "The order has " << SkidsOrder << " Skids plus one skid of " << PiecesLooseTubs << " Tubs. The order will need a total of " << TotalTubs << " Tubs." << std::endl;
+        SkidMath(Part21982.TotalAmount, Part21982.Skid, Part21982.PiecesPerTub, Part21982.TubsPerSkid);
 
 
         break;
@@ -46,13 +54,7 @@ void SubaruCalc()
         std::cout << "Enter the amount of pieces on the order for 21983" << std::endl;
         std::cin >> Part21983.TotalAmount;
 
-        SkidsOrder = Part21983.TotalAmount / Part21983.Skid;
-        PiecesLooseTubs = (Part21983.TotalAmount % Part21983.Skid) / Part21983.PiecesPerTub;
-        TotalTubs = Part21983.TotalAmount / Part21983.PiecesPerTub;
-
-        std::cout << "The order has " << SkidsOrder << " Skids plus one skid of " << PiecesLooseTubs << " Tubs. The order will need a total of " << TotalTubs << " Tubs." << std::endl;
-
-
+        SkidMath(Part21983.TotalAmount, Part21983.Skid, Part21983.PiecesPerTub, Part21983.TubsPerSkid);
 
         break;
     }
@@ -64,11 +66,8 @@ void SubaruCalc()
         std::cout << "Enter the amount of pieces on the order for 22175" << std::endl;
         std::cin >> Part22175.TotalAmount;
 
-        SkidsOrder = Part22175.TotalAmount / Part22175.Skid;
-        PiecesLooseTubs = (Part22175.TotalAmount % Part22175.Skid) / Part22175.PiecesPerTub;
-        TotalTubs = Part22175.TotalAmount / Part22175.PiecesPerTub;
+        SkidMath(Part22175.TotalAmount, Part22175.Skid, Part22175.PiecesPerTub, Part22175.TubsPerSkid);
 
-        std::cout << "The order has " << SkidsOrder << " Skids plus one skid of " << PiecesLooseTubs << " Tubs. The order will need a total of " << TotalTubs << " Tubs." << std::endl;
 
 
         break;
@@ -80,12 +79,7 @@ void SubaruCalc()
         int SkidsOrder{ 0 }; int PiecesLooseTubs{ 0 }; int TotalTubs{ 0 }; std::string delay{ "a" };
         std::cout << "Enter the amount of pieces on the order for 22176" << std::endl;
         std::cin >> Part22176.TotalAmount;
-        SkidsOrder = Part22176.TotalAmount / Part22176.Skid;
-        PiecesLooseTubs = (Part22176.TotalAmount % Part22176.Skid) / Part22176.PiecesPerTub;
-        TotalTubs = Part22176.TotalAmount / Part22176.PiecesPerTub;
-
-        std::cout << "The order has " << SkidsOrder << " Skids plus one skid of " << PiecesLooseTubs << " Tubs. The order will need a total of " << TotalTubs << " Tubs." << std::endl;
-        std::cin >> delay;
+        SkidMath(Part22176.TotalAmount, Part22176.Skid, Part22176.PiecesPerTub, Part22176.TubsPerSkid);
 
         break;
     }
@@ -96,14 +90,9 @@ void SubaruCalc()
         int SkidsOrder{ 0 }; int PiecesLooseTubs{ 0 }; int TotalTubs{ 0 }; std::string delay{ "a" };
         std::cout << "Enter the amount of pieces on the order for 22177" << std::endl;
         std::cin >> Part22177.TotalAmount;
+        
+        SkidMath(Part22177.TotalAmount, Part22177.Skid, Part22177.PiecesPerTub, Part22177.TubsPerSkid);
 
-        SkidsOrder = Part22177.TotalAmount / Part22177.Skid;
-        PiecesLooseTubs = (Part22177.TotalAmount % Part22177.Skid) / Part22177.PiecesPerTub;
-        TotalTubs = Part22177.TotalAmount / Part22177.PiecesPerTub;
-
-        std::cout << "The order has " << SkidsOrder << " Skids plus one skid of " << PiecesLooseTubs << " Tubs. The order will need a total of " << TotalTubs << " Tubs." << std::endl;
-
-        break;
     }
 
     case 22178:
@@ -113,11 +102,8 @@ void SubaruCalc()
         std::cout << "Enter the amount of pieces on the order for 22178" << std::endl;
         std::cin >> Part22178.TotalAmount;
 
-        SkidsOrder = Part22178.TotalAmount / Part22178.Skid;
-        PiecesLooseTubs = (Part22178.TotalAmount % Part22178.Skid) / Part22178.PiecesPerTub;
-        TotalTubs = Part22178.TotalAmount / Part22178.PiecesPerTub;
+        SkidMath(Part22178.TotalAmount, Part22178.Skid, Part22178.PiecesPerTub, Part22178.TubsPerSkid);
 
-        std::cout << "The order has " << SkidsOrder << " Skids plus one skid of " << PiecesLooseTubs << " Tubs. The order will need a total of " << TotalTubs << " Tubs." << std::endl;
 
 
         break;
@@ -130,12 +116,7 @@ void SubaruCalc()
         std::cout << "Enter the amount of pieces on the order for 22182" << std::endl;
         std::cin >> Part22182.TotalAmount;
 
-        SkidsOrder = Part22182.TotalAmount / Part22182.Skid;
-        PiecesLooseTubs = (Part22182.TotalAmount % Part22182.Skid) / Part22182.PiecesPerTub;
-        TotalTubs = Part22182.TotalAmount / Part22182.PiecesPerTub;
-
-        std::cout << "The order has " << SkidsOrder << " Skids plus one skid of " << PiecesLooseTubs << " Tubs. The order will need a total of " << TotalTubs << " Tubs." << std::endl;
-        break;
+        SkidMath(Part22182.TotalAmount, Part22182.Skid, Part22182.PiecesPerTub, Part22182.TubsPerSkid);
     }
 
     case 22183:
@@ -145,11 +126,7 @@ void SubaruCalc()
         std::cout << "Enter the amount of pieces on the order for 22183" << std::endl;
         std::cin >> Part22183.TotalAmount;
 
-        SkidsOrder = Part22183.TotalAmount / Part22183.Skid;
-        PiecesLooseTubs = (Part22183.TotalAmount % Part22183.Skid) / Part22183.PiecesPerTub;
-        TotalTubs = Part22183.TotalAmount / Part22183.PiecesPerTub;
-
-        std::cout << "The order has " << SkidsOrder << " Skids plus one skid of " << PiecesLooseTubs << " Tubs. The order will need a total of " << TotalTubs << " Tubs." << std::endl;
+        SkidMath(Part22183.TotalAmount, Part22183.Skid, Part22183.PiecesPerTub, Part22183.TubsPerSkid);
 
         break;
     }
